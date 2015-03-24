d8b1e1f7298ae639c6108639ae29a3fba0d74c4a
jodonne91 <jodonne91@gmail.com>
2015-03-24 14:57:21 -0400

    initial commit, styling and some functionality

--- public/index.js
+++ public/index.js
@@ -1,1 +1,45 @@
 //index.js
+
+var taskItemTemplate;
+var tasks = []
+
+var buttonClick = function(){
+
+	$('.add-task-button').on('click', function(){
+		var text = $('.new-task-input').val();
+		// console.log(text);
+		createTask(text);
+		$('.new-task-input').val('');
+		updateList();
+	})
+
+}
+
+var createTask = function(name){
+	tasks.push({text: name});
+}
+
+var updateList = function(){
+
+	$('.tasks-list').html('')
+	_.each(tasks,function(task){
+		$('.tasks-list').append(taskItemTemplate(task));
+	});
+
+	$('.mark-complete-button').on('click', function(evt){
+		console.log(evt)
+		evt.currentTarget.parentElement.remove();
+	})
+
+}
+
+$(document).on('ready',function(){
+
+	buttonClick();
+
+	var taskItemTemplateText = $('#task-item-template').html();
+	taskItemTemplate = Handlebars.compile(taskItemTemplateText);
+
+
+})
+	
