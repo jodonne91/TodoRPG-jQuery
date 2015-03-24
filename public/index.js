2a3ab540e66783689bf38c6997da50fa211a6afa
jodonne91 <jodonne91@gmail.com>
2015-03-24 15:27:10 -0400

    full functionality to task list, no rpg elements

--- public/index.js
+++ public/index.js
@@ -2,35 +2,56 @@
 
 var taskItemTemplate;
 var tasks = []
+var numberOfTasks = 0;
 
 var buttonClick = function(){
 
 	$('.add-task-button').on('click', function(){
 		var text = $('.new-task-input').val();
 		// console.log(text);
-		createTask(text);
-		$('.new-task-input').val('');
-		updateList();
-	})
+		if(text){
+			createTask(text);
+			$('.new-task-input').val('');
+			updateList();
+		}
+		else{
+			alert('Task item cannot be blank')
+		}
+	});
 
 }
 
 var createTask = function(name){
-	tasks.push({text: name});
+	numberOfTasks++;
+	tasks.push({text: name, id:numberOfTasks, status: 'active'});
 }
 
 var updateList = function(){
 
-	$('.tasks-list').html('')
+	$('.tasks-list').html('');
+
 	_.each(tasks,function(task){
 		$('.tasks-list').append(taskItemTemplate(task));
 	});
 
-	$('.mark-complete-button').on('click', function(evt){
-		console.log(evt)
-		evt.currentTarget.parentElement.remove();
-	})
+	markCompleteButton();
 
+}
+
+var markCompleteButton = function(){
+		$('.mark-complete-button').on('click', function(evt){
+		//console.log(evt)
+
+		var id = $(this).attr('id');
+		$(this).parent().addClass('complete');
+		
+		_.each(tasks, function(task){
+			if(task.id*1 === id*1){
+				task.status = 'complete';
+			}
+		})
+
+	})
 }
 
 $(document).on('ready',function(){
